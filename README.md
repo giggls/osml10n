950835b59446d04c2326dbd49c40d26ec4c601d0
Sven Geggus <sven-git@geggus.net>
2020-05-21 16:02:37 +0200

    A geolocation aware transcription daeomon written in python

---
+++ README.md
@@ -0,0 +1,83 @@
+# A new approach for Openstreetmap l10n
+
+Right after I got the maintainer of the [German Mapnik style](https://github.com/giggls/openstreetmap-carto-de)
+in 2012 I immediately thought that it would be nice to have Latin labels on
+my map rather than the respective local script.
+
+This is when the first versions of the OSMl10n functions were born.
+
+At this time implementing them in PL/pgSQL as PostgreSQL stored procedures
+seemed to be a natural choice.
+
+However starting in 2019 this approach started to show a couple of
+limitations.
+
+Many FOSS transcription libraries are written in the python language thus we
+already had to switch parts of the code to PL/Python.
+
+This started for Thai language using tltk which worked good enough. However
+trying to use this approach for Cantonese language using pinyin_jyutping_sentence
+was way too slow. Importing this library takes a couple of seconds and can
+not be done just once but must be done once per transaction.
+
+Also, we noticed that PostgreSQL has a hard coded limit for pre-compiled
+regular expressions, which we where using quite heavily for street-name
+abbreviations. Exceeding this limit will again slow down queries in an
+unacceptable way.
+
+Discussion other approaches we now came up with the following idea.  In
+future, there will be an external daemon written in python doing geolocation
+aware transcription.  This daemon has already been implemented and is part
+of this repository.  Currently missing (May 2020) are the l10n functions
+themselves which I plan to re implement in lua which will make them usable in
+osm2pgsql during import stage.
+
+At least in theory it should also be possible to call the daemon from
+PostgreSQL stored procedures, but in the meantime I think, that doing this
+stuff at import time is the better place.
+
+If you have an Idea for a better approach feel free to open an issue here.
+
+## Installation of the transcription-daemon
+
+```
+apt install python3-psycopg2
+
+pip3 install pykakasi
+pip3 install tltk
+pip3 install pinyin_jyutping_sentence
+```
+
+From Debian package or pip3:
+
+## Tests
+
+### Japan
+```
+curl --data "142/43/東京" http://localhost:8080
+Toukyou
+```
+
+### China
+```
+curl --data "130/43/東京" http://localhost:8080
+dōng jīng
+```
+
+### Thailand
+```
+curl --data "101/16/ห้องสมุดประชาชน" http://localhost:8080
+hongsamut prachachon
+```
+
+### Macau
+```
+curl --data "113.6/22.1/香港" http://localhost:8080
+hōeng góng
+```
+
+### Hongkong
+```
+curl --data "113.9/22.25/香港" http://localhost:8080
+hōeng góng
+```
---
+++ transcription-daemon/geo-transcript-srv.py
@@ -0,0 +1,197 @@
+#!/usr/bin/python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+import argparse
+# as imports are very slow parse arguments first
+parser = argparse.ArgumentParser(description='Server for transcription of names based on geolocation')
+parser.add_argument("-b", "--bindaddr", type=str, default="localhost", help="local bind address")
+parser.add_argument("-p", "--port", default=8080, help="port to listen at")
+parser.add_argument("-v", "--verbose", action='store_true', help="print verbose output")       
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-d', '--dbcon', help='PostgreSQL (psycopg2) connection string')
+group.add_argument('-s', '--sqlitefile', default='country_osm_grid.db', help='SQLITE file')
+
+args = parser.parse_args()
+
+def vout(msg):
+  if args.verbose:
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+
+import sys
+
+sys.stdout.write("Loading osml10n transcription server: ")
+sys.stdout.flush()
+vout("\n")
+
+from contextlib import contextmanager,redirect_stderr,redirect_stdout
+from os import devnull
+
+import os
+import icu
+import unicodedata
+# Kanji in JP
+import pykakasi
+# thai language in TH
+import tltk
+# Cantonese transcription
+with open(devnull, 'w') as fnull:
+  with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
+    import pinyin_jyutping_sentence
+
+def split_by_alphabet(str):
+    strlist=[]
+    target=''
+    oldalphabet=unicodedata.name(str[0]).split(' ')[0]
+    target=str[0]
+    for c in str[1:]:
+      alphabet=unicodedata.name(c).split(' ')[0]
+      if (alphabet==oldalphabet):
+        target=target+c
+      else:
+        strlist.append(target)
+        target=c
+      oldalphabet=alphabet
+    strlist.append(target)
+    return(strlist)
+
+def thai_transcript(inpstr):
+  stlist=split_by_alphabet(inpstr)
+
+  latin = ''
+  for st in stlist:
+    if (unicodedata.name(st[0]).split(' ')[0] == 'THAI'):
+      transcript=''
+      try:
+        transcript=tltk.nlp.th2roman(st).rstrip('<s/>').rstrip()
+      except:
+        sys.stderr.write("tltk error transcribing >%s<\n" % st)
+        return(None)
+      latin=latin+transcript
+    else:
+      latin=latin+st
+  return(latin)
+
+class transcriptor:
+  def __init__(self):
+
+    # ICU transliteration instance
+    self.icutr = icu.Transliterator.createInstance('Any-Latin').transliterate
+
+    # Kanji to Latin transcription instance via pykakasi
+    kakasi = pykakasi.kakasi()
+    kakasi.setMode("H","a")
+    kakasi.setMode("K","a")
+    kakasi.setMode("J","a")
+    kakasi.setMode("r","Hepburn")
+    kakasi.setMode("s", True)
+    kakasi.setMode("E", "a")
+    kakasi.setMode("a", None)
+    kakasi.setMode("C", True)
+    self.kakasi_converter  = kakasi.getConverter()
+  
+  def transcript(self, country, unistr):
+    if country == 'jp':
+      return(self.kakasi_converter.do(unistr))
+    
+    if country == 'th':
+      return(thai_transcript(unistr))
+      
+    if country in ['mo','hk']:
+      return(pinyin_jyutping_sentence.jyutping(unistr, spaces=True))
+  
+    return(unicodedata.normalize('NFC', self.icutr(unistr)))
+
+# convert lon/lat to countrycode via PostgreSQL
+class Coord2Country_psql:
+  def __init__(self):
+      import psycopg2
+      self.sql = """
+      SELECT country_code from country_osm_grid
+      WHERE st_contains(geometry, st_transform(ST_GeomFromText('POINT(%s %s)', 4326),4326))
+      ORDER BY area LIMIT 1;
+      """
+      self.conn = psycopg2.connect(args.dbcon)
+      self.cur = self.conn.cursor()
+  def getCountry(self,lon,lat):
+    try:
+      self.cur.execute(self.sql % (lon,lat))
+      rows = self.cur.fetchall()
+      return(rows[0][0])
+    except Exception as e:
+      sys.stderr.write("Database query error:\n")
+      sys.stderr.write(str(e))
+      sys.exit(1)
+
+# convert lon/lat to countrycode via SQLITE
+class Coord2Country_sqlite:
+  def __init__(self):
+    # check if sqlite file is available
+    fn = os.path.realpath(args.sqlitefile)
+    if not os.path.isfile(fn):
+      sys.stderr.write("Unable to open SQLITE file: %s\n" % args.sqlitefile)
+      sys.exit(1)
+    import sqlite3
+    self.sql = """
+    SELECT country_code from country_osm_grid
+    WHERE st_contains(geometry, st_transform(ST_GeomFromText('POINT(%s %s)', 4326),4326))
+    ORDER BY area LIMIT 1;
+    """
+    self.conn = sqlite3.connect(fn)
+    self.conn.enable_load_extension(True)
+    self.conn.load_extension("mod_spatialite")
+    self.cur = self.conn.cursor()
+
+  def getCountry(self,lon,lat):
+    self.cur.execute(self.sql % (lon,lat))
+    rows = self.cur.fetchall()
+    return(rows[0][0])
+
+# convert lon/lat to countrycode via PostgreSQL
+class Coord2Country:
+  def __init__(self):
+    if args.dbcon is not None:
+      vout("Using PostgreSQL for country_osm_grid!\n")
+      self.co2c = Coord2Country_psql()
+    else:
+      vout("Using SQLITE for country_osm_grid!\n")
+      self.co2c = Coord2Country_sqlite()
+  def getCountry(self,lon,lat):
+    country = self.co2c.getCountry(lon,lat)
+    vout("country for %s/%s is: %s\n" % (lon,lat,country))
+    return(country)
+
+class httpServer(BaseHTTPRequestHandler):
+
+  co2c=Coord2Country()
+  tc=transcriptor()
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/plain")
+    self.end_headers()
+    self.wfile.write(b"This server is POST only!\n")
+
+  def do_POST(self):
+    content_length = int(self.headers['Content-Length'])
+    post_data = self.rfile.read(content_length).decode('utf-8')
+    (lon,lat,name) = post_data.split('/',2)
+    cc=httpServer.co2c.getCountry(lon,lat)
+    self.wfile.write(bytes(httpServer.tc.transcript(cc,name),"utf-8"))
+
+  def log_message(self, format, *args):
+    return
+
+
+
+if __name__ == "__main__":
+  webServer = HTTPServer((args.bindaddr, args.port), httpServer)
+
+  sys.stdout.write("ready.\n")
+  try:
+    webServer.serve_forever()
+  except KeyboardInterrupt:
+    pass
+
+  webServer.server_close()
